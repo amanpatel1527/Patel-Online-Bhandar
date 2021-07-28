4e115df373e58fc88783580e4402d692a48c56e6
amanpatel1527 <patelpatelaman00@gmail.com>
2021-07-28 11:56:22 +0530

    Online-Bhandar

---
+++ script/index.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector(".hamburger");
+const navlist = document.querySelector(".nav-list");
+
+if (hamburger) {
+  hamburger.addEventListener("click", () => {
+    navlist.classList.toggle("open");
+  });
+}
+
+// Popup
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup-close");
+
+if (popup) {
+  closePopup.addEventListener("click", () => {
+    popup.classList.add("hide-popup");
+  });
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      popup.classList.remove("hide-popup");
+    }, 1000);
+  });
+}
---
+++ script/slider.js
@@ -0,0 +1,15 @@
+//  hero Carousel
+
+const slider1 = document.querySelector("#glide1");
+if (slider1) {
+  new Glide (slider1, {
+    type: "carousel",
+    startAt: 0,
+    autoplay: 3000,
+    gap: 0,
+    hoverpause: true,
+    perView: 1,
+    animationDuration: 800,
+    AnimationTimingFunc: "linear",
+  }).mount()
+}
